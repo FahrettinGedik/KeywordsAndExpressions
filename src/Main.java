567476c45996b4f2d45146524ca665ebe464616c
Fahrettin Gedik <fahged41@gmail.com>
2024-12-31 23:12:06 +0100

    StatementsWhiteSpaceAndIndenting  are added.

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,20 @@
 public class Main {
+
+    public static void main(String[] args) {
+        double kilometer = (100 * 1.609344);
+
+        int highScore = 50;
+
+        if (highScore > 25) {
+            highScore = 1000 + highScore; // add bonus points
+        }
+
+        int health = 100;
+
+        if ((health < 50) && (health > 1000)){
+            highScore = highScore - 1000;
+        }
+
+    }
+
 }
---
+++ src/StatementsWhiteSpaceAndIndenting.java
@@ -0,0 +1,17 @@
+public class StatementsWhiteSpaceAndIndenting {
+
+    public static void main(String[] args) {
+
+        int myVariable = 50;
+
+        myVariable++;
+        myVariable--;
+
+        System.out.println("This is a test");
+
+        if (myVariable ==  0) {
+            System.out.println("It's now zero");
+        }
+
+    }
+}
